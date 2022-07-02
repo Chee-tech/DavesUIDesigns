373fcc2d100eb448934cee2b1a588121b5f6bb0e
FlutLab.io <>
2022-07-02 13:11:57 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/recruitment_20appapp/generatediphone11prox1widget/GeneratedIPhone11ProX1Widget.dart';
+import 'package:flutterapp/recruitment_20appapp/generatedheartwidget/GeneratedHeartWidget.dart';
+import 'package:flutterapp/recruitment_20appapp/generatedrectangle3widget/GeneratedRectangle3Widget.dart';
+import 'package:flutterapp/recruitment_20appapp/generatediphone11prox2widget/GeneratedIPhone11ProX2Widget.dart';
+import 'package:flutterapp/recruitment_20appapp/generatediphone11prox3widget/GeneratedIPhone11ProX3Widget.dart';
+import 'package:flutterapp/recruitment_20appapp/generatediphone11prox4widget/GeneratedIPhone11ProX4Widget.dart';
+import 'package:flutterapp/recruitment_20appapp/generatedmanagersseeusasareliablestandbyprofessionalstaffoptiona/GeneratedManagersseeusasaReliableStandbyProfessionalstaffoptiona.dart';
+import 'package:flutterapp/recruitment_20appapp/generatediphone11prox5widget/GeneratedIPhone11ProX5Widget.dart';
+import 'package:flutterapp/recruitment_20appapp/generatediphone11prox7widget/GeneratedIPhone11ProX7Widget.dart';
+import 'package:flutterapp/recruitment_20appapp/generatediphone11prox6widget/GeneratedIPhone11ProX6Widget.dart';
+import 'package:flutterapp/recruitment_20appapp/generatedcccccccccccccccccccccc1widget/GeneratedCccccccccccccccccccccc1Widget.dart';
+
+void main() {
+  runApp(RECRUITMENT_20APPApp());
+}
+
+class RECRUITMENT_20APPApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedIPhone11ProX1Widget',
+      routes: {
+        '/GeneratedIPhone11ProX1Widget': (context) =>
+            GeneratedIPhone11ProX1Widget(),
+        '/GeneratedHeartWidget': (context) => GeneratedHeartWidget(),
+        '/GeneratedRectangle3Widget': (context) => GeneratedRectangle3Widget(),
+        '/GeneratedIPhone11ProX2Widget': (context) =>
+            GeneratedIPhone11ProX2Widget(),
+        '/GeneratedIPhone11ProX3Widget': (context) =>
+            GeneratedIPhone11ProX3Widget(),
+        '/GeneratedIPhone11ProX4Widget': (context) =>
+            GeneratedIPhone11ProX4Widget(),
+        '/GeneratedManagersseeusasaReliableStandbyProfessionalstaffoptiona':
+            (context) =>
+                GeneratedManagersseeusasaReliableStandbyProfessionalstaffoptiona(),
+        '/GeneratedIPhone11ProX5Widget': (context) =>
+            GeneratedIPhone11ProX5Widget(),
+        '/GeneratedIPhone11ProX7Widget': (context) =>
+            GeneratedIPhone11ProX7Widget(),
+        '/GeneratedIPhone11ProX6Widget': (context) =>
+            GeneratedIPhone11ProX6Widget(),
+        '/GeneratedCccccccccccccccccccccc1Widget': (context) =>
+            GeneratedCccccccccccccccccccccc1Widget(),
+      },
+    );
+  }
+}
